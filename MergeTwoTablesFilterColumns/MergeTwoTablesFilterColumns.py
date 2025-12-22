35f80f11cbc9d03ab3b3bf3b8504345849043435
Danish Khan <danish.dtp@gmail.com>
2025-12-22 11:54:20 +0530

    updated scripts and readme

--- MergeTwoTablesFilterColumns/MergeTwoTablesFilterColumns.py
+++ MergeTwoTablesFilterColumns/MergeTwoTablesFilterColumns.py
@@ -1,39 +1,42 @@
-#python3
-'''
+# python3
+# MergeTwoTablesFilterColumns.pyarrow
+"""
 The script is created to match two large csv files and find the rows in the second file not available in first file.
 The script is created using dask and pyarrow libraries of script to handle large data on a low end hardware - home pc or laptop. The script has been tested with 2 GBs of csv file and works instantly.
-'''
+"""
 
 import dask.dataframe as dd
+
 
 def process_df(df, id_col, ref_col):
     # Drop rows with missing values and create 'uniqueid'
     df = df.dropna(subset=[id_col])
-    df['uniqueid'] = 'id' + df['familyid'].astype(str) + df[id_col].astype(str)
-    return df[['uniqueid','familyid','memberid']]
+    df["uniqueid"] = "id" + df["familyid"].astype(str) + df[id_col].astype(str)
+    return df[["uniqueid", "familyid", "memberid"]]
+
 
 # Load and process the first CSV file
-df1 = dd.read_csv('rcms.csv')
-df1 = process_df(df1, 'memberid', 'familyid')
-#print (df1.head(10))
+df1 = dd.read_csv("rcms.csv")
+df1 = process_df(df1, "memberid", "familyid")
+# print (df1.head(10))
 # Save intermediate result
-#df1.to_csv('rcms2-*.csv', index=False, single_file=True)
+# df1.to_csv('rcms2-*.csv', index=False, single_file=True)
 
 # Load and process the second CSV file
-df2 = dd.read_csv('spr.csv')
-df2 = process_df(df2, 'memberid', 'familyid')
-#print (df2.head(10))
+df2 = dd.read_csv("spr.csv")
+df2 = process_df(df2, "memberid", "familyid")
+# print (df2.head(10))
 # Save intermediate result
-#df2.to_csv('spr4-*.csv', index=False, single_file=True)
+# df2.to_csv('spr4-*.csv', index=False, single_file=True)
 
 # Merge the two DataFrames
-output1 = dd.merge(df1, df2, on='uniqueid', how='left')
+output1 = dd.merge(df1, df2, on="uniqueid", how="left")
 
-print (output1.head(10))
+print(output1.head(10))
 # Trigger computation to complete the merge and get the filtered result
-output1_filtered = output1[output1['memberid_y'].isnull()]
+output1_filtered = output1[output1["memberid_y"].isnull()]
 
 # Show the first 10 rows of the filtered output
 print(output1_filtered.head(10))
 # Save the final output
-output1_filtered.to_csv('notMatchingMembers.csv', index=False, single_file=True)
+output1_filtered.to_csv("notMatchingMembers.csv", index=False, single_file=True)
--- MergeTwoTablesFilterColumns/README.md
+++ MergeTwoTablesFilterColumns/README.md
@@ -2,15 +2,32 @@
 # Merge Two tables 
 
 ## 🛠️ Description
+The script is created to match two large csv files and find the rows in the second file not available in first file.\
+The script is created using dask and pyarrow libraries of script to handle large data on a low end hardware - home pc or laptop. The script has been tested with 2 GBs of csv file and works instantly.\
+
+## 🛠️Requirements
+
+    1. First table with large data in one csv file, this file will be on left side of left join 
+    2. Second table in one csv file, this file rows will be matched with first file.
+    3. PC/Laptop with >8 core processors and >16 GB RAM to run the script
+    4. MS Windows or Linux operating system with support for python environment
+The hardware requirements are of indicative nature. The low end hardware will also run the script successfully albeit with slower processing. I have ran the script on my personal laptop with dual core processor and 4 GB RAM on linux operating system.\
+Hence, there are no expensive servers or frameworks are required for this project.\
 
 ## ⚙️ Languages or Frameworks Used
-
+Following python libraries are required to be installed\
+    * pandas
+    * pyarrow
+    * dask
+    * numpy
+    * xlrd
+To install using pip run following :\
+`pip install pandas numpy xlrd openpyxl dask pyarrow`\
 
 ## 💾How to run:
-
-## 📁Sample file to test the script
-
-## 🏗 Project structure
+To run the script you need to specify the file names with address into the script. It's easier if both files are present in the same directory/folder as the script.\
+After this, run following command in terminal:\
+`python MergeTwoTablesFilterColumns.py`
 
 ## 💡 Other modification ideas:
 
