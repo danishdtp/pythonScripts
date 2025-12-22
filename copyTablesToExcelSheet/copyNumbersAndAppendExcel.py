ffa53cd7713ad52b53d452b282161d42f704cd83
Danish Khan <danish.dtp@gmail.com>
2025-12-22 11:05:53 +0530

    updated readme

--- copyTablesToExcelSheet/copyNumbersAndAppendExcel.py
+++ copyTablesToExcelSheet/copyNumbersAndAppendExcel.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-append_8digit_to_sheet2.py
+copyNumbersAndAppendExcel.py
 - Reads text from the system clipboard
 - Finds all distinct 8-digit numbers
 - Appends them (one per row) to Sheet2 of the specified Excel .xlsx file
