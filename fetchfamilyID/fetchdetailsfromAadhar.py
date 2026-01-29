53cd18596b39ac6828d536320df813f952678b9b
Danish Khan <danish.dtp@gmail.com>
2026-01-29 06:42:43 +0530

    updated script to add excel file

--- fetchfamilyID/fetchdetailsfromAadhar.py
+++ fetchfamilyID/fetchdetailsfromAadhar.py
@@ -30,75 +30,83 @@ def process_12digit_number(df, cells, output_file_name):
     print("Total : ", total_count)
     count = 0
     start_time = time.perf_counter()
-    for index, col, value in cells:
-        # 1) Copy the 8-digit number to clipboard
-        count += 1
-        print(f"S.No {count}/{total_count}", end=" ")
+    try:
+        for index, col, value in cells:
+            # 1) Copy the 8-digit number to clipboard
+            count += 1
+            print(f"S.No {count}/{total_count}", end=" ")
+            if pd.notna(df.loc[index, "Samagra"]):
+                continue
 
-        pyperclip.copy(value)
-        # 2) Open a website (URL needs to be specified)
-        # web_url = (
-        #     "https://epos.mp.gov.in/RC_Mobile_Int.jsp"  # Change to the desired URL
-        # )
-        # pyautogui.hotkey("ctrl", "l")  # Focus address bar
-        # time.sleep(0.2)  # Wait for a second
-        # pyautogui.write(web_url)
-        # pyautogui.press("enter")
+            pyperclip.copy(value)
+            # 2) Open a website (URL needs to be specified)
+            # web_url = (
+            #     "https://epos.mp.gov.in/RC_Mobile_Int.jsp"  # Change to the desired URL
+            # )
+            # pyautogui.hotkey("ctrl", "l")  # Focus address bar
+            # time.sleep(0.2)  # Wait for a second
+            # pyautogui.write(web_url)
+            # pyautogui.press("enter")
 
-        # Wait for the page to load (you may need to adjust this)
-        pyautogui.hotkey("home")
-        time.sleep(0.2)
-        pyautogui.click(x=971, y=307)  # Replace with actual coordinates
-        time.sleep(0.2)
-        pyautogui.click(x=971, y=307)  # Replace with actual coordinates
-        time.sleep(0.2)
-        # 3) Paste value in a search field using pyautogui
-        pyautogui.hotkey("ctrl", "v")
-        time.sleep(0.2)
-        # pyautogui.write(value)
+            # Wait for the page to load (you may need to adjust this)
+            pyautogui.click(x=960, y=388)  # Replace with actual coordinates
+            pyautogui.hotkey("home")
+            time.sleep(0.2)
+            pyautogui.click(x=960, y=388)  # field
+            time.sleep(0.2)
+            pyautogui.click(x=960, y=388)  # field
+            time.sleep(0.2)
+            # 3) Paste value in a search field using pyautogui
+            pyautogui.hotkey("ctrl", "v")
+            time.sleep(0.2)
+            # pyautogui.write(value)
 
-        # 4) Click on the submit button (coordinates need to be adjusted)
-        pyautogui.click(x=1121, y=305)
+            # 4) Click on the submit button (coordinates need to be adjusted)
+            pyautogui.click(x=1121, y=379)  # Submit button
 
-        # 5) Wait for 2 seconds
-        time.sleep(10)
-        pyautogui.click(x=1157, y=599)  # Replace with actual coordinates
-        # 6) Drag to select the text
-        time.sleep(0.2)
-        pyautogui.hotkey("ctrl", "a")
-        time.sleep(0.3)
-        # 7) Copy that text
-        pyautogui.hotkey("ctrl", "c")
-        # 8) Wait a moment for the clipboard to update
-        time.sleep(0.3)
-        copied_text = pyperclip.paste()
+            # 5) Wait for 2 seconds
+            time.sleep(10)
+            pyautogui.click(x=1160, y=601)  # OK button
+            # 6) Drag to select the text
+            time.sleep(0.2)
+            pyautogui.hotkey("ctrl", "a")
+            time.sleep(0.3)
+            # 7) Copy that text
+            pyautogui.hotkey("ctrl", "c")
+            # 8) Wait a moment for the clipboard to update
+            time.sleep(0.3)
+            copied_text = pyperclip.paste()
 
-        # 9) Find a 10-digit number in that copied text
-        match = re.search(r"\b\d{8}\b", copied_text)
-        fps = re.search(r"\b\d{7}\b", copied_text)
-        if match:
-            found_number = match.group(0)
-            df.loc[index, "Samagra"] = found_number
-            if fps:
-                fps = fps.group(0)
-                df.loc[index, "fps"] = fps
-            # Add the found digit number into a new column in the DataFrame
-            print(f"{count}/{total_count} - {value},  - {found_number}")
-        elif "Details not found for" in copied_text:
-            print(f"RC Number {value} is not valid")
-        else:
-            df.loc[index, "Mobile_Number"] = None
-            print(f"{count}/{total_count} - {value},  - NA")
+            # 9) Find a 10-digit number in that copied text
+            match = re.search(r"\b\d{8}\b", copied_text)
+            fps = re.search(r"\b\d{7}\b", copied_text)
+            if match:
+                found_number = match.group(0)
+                df.loc[index, "Samagra"] = found_number
+                if fps:
+                    fps = fps.group(0)
+                    df.loc[index, "FPS"] = fps
+                # Add the found digit number into a new column in the DataFrame
+                print(f"{count}/{total_count} - {value},  - {found_number}")
+            elif "Details not found for" in copied_text:
+                df.loc[index, "Samagra"] = "Not found"
+                df.loc[index, "FPS"] = "Not found"
 
-        one_time = time.perf_counter()
-        global estimate
-        if count == 1:
-            estimate = (one_time - start_time) * total_count
-            print(f"Estimated time : {estimate / 60:.1f},minutes")
-        elif count % 10 == 0:
-            remaining_time = estimate - (one_time - start_time)
-            print(f"Remaining time : {remaining_time / 60:.1f},minutes")
+                print(f"RC Number {value} is not valid")
+            else:
+                # df.loc[index, "Mobile_Number"] = None
+                print(f"{count}/{total_count} - {value},  - NA")
+
+            one_time = time.perf_counter()
+            df.to_excel(output_file_name, index=False)
+    except KeyboardInterrupt:
+        print("Exiting")
         df.to_excel(output_file_name, index=False)
+        sys.exit()
+    except pyautogui.FailSafeException:
+        print("Exiting Failsafe")
+        df.to_excel(output_file_name, index=False)
+        sys.exit()
     return df
 
 
